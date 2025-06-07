a52fedd7f8cc0454a2c1e20f7d6afc23cab4d1e0
NeeharSatti1998 <sattineehar@gmail.com>
2025-06-06 23:33:09 -0400

    Add grafana visualizations

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from prometheus_fastapi_instrumentator import Instrumentator
 from prometheus_fastapi_instrumentator.metrics import latency
 
 app = FastAPI()
-Instrumentator().instrument(app).expose(app)
+Instrumentator().add(latency()).instrument(app).expose(app)
 
 # --- S3 Model Loading ---
 MODEL_DIR = "/tmp/model"
